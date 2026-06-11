94c4db12e9057d7e51f752685d66b19702769ee0
Kaan <kaan-yalcinkaya66@hotmail.com>
2026-06-11 22:40:23 +0300

    xx

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,12 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by(
+                $request->user()?->id ?: $request->ip()
+            );
+        });
+
         RateLimiter::for('auth-api', function (Request $request) {
             return [
                 Limit::perMinute(10)->by($request->ip()),
